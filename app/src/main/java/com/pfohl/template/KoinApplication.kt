cdacbd9a0a70a84c3b26a5c4a7643287df2d1f2f
randy <randypfohl@gmail.com>
2019-10-12 22:53:56 -0700

    removing build src versioning in favor of gradle versions, resolved some dependency conflicts, applied spotless kotlin, fixed example tests

--- app/src/main/java/com/pfohl/template/KoinApplication.kt
+++ app/src/main/java/com/pfohl/template/KoinApplication.kt
@@ -6,7 +6,7 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
 class KoinApplication : Application() {
-    override fun onCreate(){
+    override fun onCreate() {
         super.onCreate()
         startKoin {
             androidContext(this@KoinApplication)
--- app/src/main/java/com/pfohl/template/di/Modules.kt
+++ app/src/main/java/com/pfohl/template/di/Modules.kt
@@ -3,5 +3,4 @@ package com.pfohl.template.di
 import org.koin.dsl.module
 
 val KoinModule = module {
-
 }
